eabf8a30d85b09293d53eb6c2c2b0eda99a36ea2
Sangwook Lee <leesang627@gmail.com>
2021-11-06 10:43:56 +0900

    Make Grid in Working time section

--- apps/working_time.py
+++ apps/working_time.py
@@ -2,9 +2,17 @@ from dash import dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 from app import app
 
+
+def generate_item(title, children='children'):
+    return html.Div(
+        [html.H4(title),
+         children]
+    )
+
+
 layout = dbc.Container([
-    dbc.Row([dbc.Col(html.Div('Working places on a map')),
-            dbc.Col(html.Div('My working places'))]),
-    dbc.Row([dbc.Col(html.Div('Entire time spent at Out Lab')),
-            dbc.Col(html.Div('Daily time spent at Out Lab'))])
-])
+    dbc.Row([dbc.Col(generate_item(title='Working places on a map')),
+            dbc.Col(generate_item(title='My working places'))], class_name='h-50'),
+    dbc.Row([dbc.Col(generate_item(title='Entire time spent at Out Lab')),
+            dbc.Col(generate_item(title='Daily time spent at Out Lab'))], class_name='h-50')
+], class_name='h-100')
