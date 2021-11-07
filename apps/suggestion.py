a0422698bb6b13f7e1344cccc1fc61da2ee6b7cc
jdaria <daria@kaist.ac.kr>
2021-11-08 06:40:58 +0900

    Extract and show location&time info from hoverData

--- apps/suggestion.py
+++ apps/suggestion.py
@@ -4,12 +4,19 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from app import app
+import json
 heat_df=pd.read_csv('data/heat.csv')
-print(heat_df)
+def ampm(x):
+    if x<12:
+        res=f'{x}AM'
+    else:
+        res=f'{x}PM'
+    return res
 fig=go.Figure(data=go.Heatmap(x=heat_df.hour+1,
                               y=heat_df.location,
                               z=heat_df.rate,
-                              colorscale='sunset'))
+                              colorscale='Purp'))
+
 layout = dbc.Container([
     dbc.Row(html.Div(), class_name='h-15'),
     dbc.Row(
@@ -18,12 +25,28 @@ layout = dbc.Container([
                 html.H2("Focus Time Rate"),
                 html.P(["Summary of Focus Time Rate for selected date range.", html.Br(),
                         "Put the cursor on each slot to check app usage percentage."]),
-                dcc.Graph(figure=fig)
+                dcc.Graph(id='heatmap', figure=fig)
             ])),
             dbc.Col(html.Div([
-                html.H2("App usage"), "pie chart"
+                html.H2("App usage"),
+                html.Div(id='output')
             ]))
         ], class_name='h-70'
     ),
     dbc.Row(html.Div(), class_name='h-15')
 ], class_name="h-100")
+
+@app.callback(
+    Output('output', 'children'),
+    [Input('heatmap', 'hoverData'), Input('heatmap','clickData')])
+def show_data(hoverData, clickData):
+    if (hoverData is not None):
+        locationName=hoverData['points'][0]['y']
+        timeMid=hoverData['points'][0]['x']
+        timeStart=ampm(timeMid-1)
+        timeEnd=ampm(timeMid+1)
+        return [
+            f'{locationName}, {timeStart} - {timeEnd}'
+        ]
+    else:
+        return []
