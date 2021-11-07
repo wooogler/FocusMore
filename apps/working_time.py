079861424adc549560364ec1ca278bd64898b91c
Sangwook Lee <leesang627@gmail.com>
2021-11-07 21:27:10 +0900

    fix total_still_time, add total_wasting_time

--- apps/working_time.py
+++ apps/working_time.py
@@ -10,11 +10,8 @@ def generate_item(title, children="children"):
     return html.Div([html.H4(title), children], style={"height": "100%"})
 
 
-df_act = pd.read_csv(
-    "data/PhysicalActivityTransitionEntity-5572736000.csv", index_col="timestamp"
-)
-df_act["timestamp"] = pd.to_datetime(df_act.index, unit="ms")
-df_act.set_index("timestamp", drop=True, inplace=True)
+df_act = pd.read_csv("data/PhysicalActivityTransitionEntity-5572736000.csv")
+df_dev = pd.read_csv("data/DeviceEventEntity-5572736000.csv")
 
 # map
 df_loc = pd.read_csv("data/LocationEntity-5572736000.csv", index_col="timestamp")
@@ -106,6 +103,40 @@ def grouper(list_, threshold):
         yield group
 
 
+def get_still_time(df):
+    still_time = 0
+    if not isinstance(df, pd.DataFrame):
+        return still_time
+    df = df.drop_duplicates(["timestamp", "transitionType"], keep="first")
+    df = df.loc[df["transitionType"].isin(["ENTER_STILL", "EXIT_STILL"])]
+    for idx, row in df.iterrows():
+        if row["transitionType"] == "ENTER_STILL":
+            enter_still_time = row["timestamp"]
+        elif row["transitionType"] == "EXIT_STILL":
+            exit_still_time = row["timestamp"]
+            if enter_still_time in locals():
+                still_time = still_time + exit_still_time - enter_still_time
+
+    return still_time
+
+
+def get_screen_time(df):
+    screen_time = 0
+    if not isinstance(df, pd.DataFrame):
+        return screen_time
+    df = df.drop_duplicates(["timestamp", "type"], keep="first")
+    df = df.loc[df["type"].isin(["SCREEN_ON", "SCREEN_OFF"])]
+    for idx, row in df.iterrows():
+        if row["type"] == "SCREEN_ON":
+            screen_on_time = row["timestamp"]
+        elif row["type"] == "SCREEN_OFF":
+            screen_off_time = row["timestamp"]
+            if screen_on_time in locals():
+                screen_time = screen_time + screen_off_time - screen_on_time
+
+    return screen_time
+
+
 @app.callback(
     Output("entire-chart", "figure"), Input("map-interactions", "selectedData")
 )
@@ -125,31 +156,32 @@ def display_selected_data(selectedData):
         )
     )
     # print(len(timestamp_range), timestamp_range)
-    still_time = 0
+    total_still_time = 0
+    total_screen_time = 0
     total_time = 0
-    df_act_list = []
     for range in timestamp_range:
-        # print(pd.to_datetime(range[0], unit="ms"), pd.to_datetime(range[1], unit="ms"))
         total_time = total_time + range[1] - range[0]
-        # print(total_time)
-        df_act_list.append(
-            df_act.loc[
-                pd.to_datetime(range[0], unit="ms") : pd.to_datetime(
-                    range[1], unit="ms"
-                )
-            ]
-        )
+        df_act_place = df_act[df_act["timestamp"].between(range[0], range[1])]
+        df_dev_place = df_dev[df_dev["timestamp"].between(range[0], range[1])]
+        total_still_time = total_still_time + get_still_time(df_act_place)
+        total_screen_time = total_screen_time + get_screen_time(df_dev_place)
     # 여기 나중에 해결하기
-    print(df_act_list)
-
-    for df_act_item in df_act_list:
-        for idx, item in df_act_item.iterrows():
-            if item["transitionType"] == "ENTER_STILL":
-                enter_still_timestamp = int(idx)
-            elif item["transitionType"] == "EXIT_STILL":
-                exit_still_timestamp = int(idx)
-                still_time = still_time + (exit_still_timestamp - enter_still_timestamp)
-    print(timestamp_range)
-    print(still_time / (1000 * 60 * 60))
     print(total_time / (1000 * 60 * 60))
-    return ""
+    print(total_still_time / (1000 * 60 * 60))
+    print(total_screen_time / (1000 * 60 * 60))
+    focus_time = total_still_time - total_screen_time
+    leisure_time = total_time - total_still_time
+    wasting_time = total_screen_time
+    df_entire = pd.DataFrame(
+        pd.Series(
+            {
+                "Focus Time": focus_time,
+                "Wasting Time": wasting_time,
+                "Leisure Time": leisure_time,
+            }
+        ),
+        columns=["time"],
+    )
+    fig = px.pie(df_entire, values="time", names=df_entire.index)
+    # pie 차트 안 나옴
+    return fig
