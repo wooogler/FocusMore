21d8d646233716ecf114f2a54abbab3b48928280
jdaria <daria@kaist.ac.kr>
2021-11-16 13:40:33 +0900

    working on layouts

--- app.py
+++ app.py
@@ -4,8 +4,5 @@ from dash import dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 app = dash.Dash(__name__, suppress_callback_exceptions=True,
                 external_stylesheets=[dbc.themes.BOOTSTRAP])
-app.layout = html.Div(
-    [dcc.Location(id="url", refresh=False), sidebar, navbar, content],
-    style={"height": "100vh"},
-)
+app.layout = html.Div(id='app-layout', style={"height": "100vh"})
 server = app.server
--- index.py
+++ index.py
@@ -103,7 +103,7 @@ content = html.Div(id="page-content", style=CONTENT_STYLE)
 
 app.layout = html.Div(
     [dcc.Location(id="url", refresh=False), sidebar, navbar, content],
-    style={"height": "100vh"},
+    style={"height": "100vh"}, id='index-layout'
 )
 
 
@@ -130,6 +130,11 @@ def render_page_content(pathname):
         className="py-3",
     )
 
-
+@app.callback(
+    Output("index-layout", "children"),
+    Input("app-layout", "children")
+)
+def set_layout(lo):
+    return lo
 if __name__ == "__main__":
     app.run_server(port=8888, debug=True)
