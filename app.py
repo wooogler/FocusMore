f9b7018060ef9101a74fe615f4b03c3d18a8afd7
jdaria <daria@kaist.ac.kr>
2021-11-16 13:19:34 +0900

    write test text for app layout

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True,
                 external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.layout=html.Div(["home"])
 server = app.server
