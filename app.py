8242f16139569ef4188ff5ab9954893c3fe15755
jdaria <daria@kaist.ac.kr>
2021-11-16 12:17:51 +0900

    m

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import dash
 import dash_bootstrap_components as dbc
+const PORT = proceess.env.PORT || '8080'
 
+app.set("port", PORT)
 app = dash.Dash(__name__, suppress_callback_exceptions=True,
                 external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
