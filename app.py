2fcbc8dff4a89f93d4fcfb861dcafa4f98ee1cd6
jdaria <daria@kaist.ac.kr>
2021-11-16 13:31:17 +0900

    test layout of app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import dash
 import dash_bootstrap_components as dbc
-from dash import html
-
+from dash import dcc, html, Input, Output
+import dash_bootstrap_components as dbc
 app = dash.Dash(__name__, suppress_callback_exceptions=True,
                 external_stylesheets=[dbc.themes.BOOTSTRAP])
-app.layout=html.Div(["home"])
+app.layout = html.Div(
+    [dcc.Location(id="url", refresh=False), sidebar, navbar, content],
+    style={"height": "100vh"},
+)
 server = app.server
