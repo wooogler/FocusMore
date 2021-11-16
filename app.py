d920f35d1d09402a41509741e32b5d1a9c15b830
jdaria <daria@kaist.ac.kr>
2021-11-16 13:23:58 +0900

    edit index.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import dash
 import dash_bootstrap_components as dbc
-
+from dash import html
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True,
                 external_stylesheets=[dbc.themes.BOOTSTRAP])
